560cef19b448ee564bd29f91deae0b8813009758
Mikhil Mohan C <mikhilmohan13@gmail.com>
2026-01-30 15:01:52 +0530

    chore: Update base URL constant.

--- lib/core/constants/app_urls.dart
+++ lib/core/constants/app_urls.dart
@@ -1,5 +1,5 @@
 class AppUrls {
-  static const String baseUrl = 'https://k75h4jwq-8002.inc1.devtunnels.ms';
+  static const String baseUrl = 'https://4vwmk0fb-8003.inc1.devtunnels.ms';
   static const String userUrl = '$baseUrl/user';
 
   static const String loginUrl = '$userUrl/login/';
